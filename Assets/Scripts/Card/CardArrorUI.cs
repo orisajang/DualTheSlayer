fc962f1cf15c0f5509bbce9b050a8f626fec268d
orisajang <white928620@gmail.com>
2026-01-16 15:22:54 +0900

    [feat] 카드 드래그로 적 선택만 가능하도록 추가

--- Assets/Scripts/Card/CardArrorUI.cs
+++ Assets/Scripts/Card/CardArrorUI.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class CardArrorUI : MonoBehaviour,
-    IPointerDownHandler, IDragHandler, IPointerUpHandler
+    IPointerDownHandler, IDragHandler, IPointerUpHandler,
+    IEndDragHandler
 {
     //카드를 클릭하면 화살표UI가 나와서 타게팅 가능하도록
     UITargetArrow arrow;
@@ -14,6 +15,9 @@ public class CardArrorUI : MonoBehaviour,
     //카드 데이터를 얻어오기위해서
     [SerializeField] CardView cardView;
     eTargetType targetType;
+
+    //클릭 가능한 대상 타겟팅
+    [SerializeField] LayerMask _layer;
 
     void Awake()
     {
@@ -46,6 +50,25 @@ public class CardArrorUI : MonoBehaviour,
         if (!IsDragAble()) return;
         arrow.Show(rect.position, eventData.position);
     }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //드래그가 끝났을때 사용. (Up에서 실제로 드래그가 발생했을때만 호출하기위해서 EndDrag사용)
+        Ray ray = Camera.main.ScreenPointToRay(eventData.position);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit,100f,_layer))
+        {
+            Debug.Log("앞에 물체가있다! 이름:" + hit.collider.name);
+            Debug.Log("거리: " + hit.distance);
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+            if(enemy != null)
+            {
+                Debug.Log("준비 완료");
+            }
+
+        }
+
+    }
 
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -60,5 +83,7 @@ public class CardArrorUI : MonoBehaviour,
         //현재 자신의 ID로 자신의 턴인지 확인한다
         return GameManager.Instance.turnManager.IsMyTurn(GameManager.Instance.playerManager.PlayerID);
     }
+
+    
 }
 
